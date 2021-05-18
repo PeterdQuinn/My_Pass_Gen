7638a6b0ac620fdf993426610bad83bcb6bdd29e
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2021-05-18 01:11:32 -0700

    Main

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,1 +1,27 @@
 /////---START PASS WORD APPLICATION----////
+
+var generatePassword = function() {
+
+
+var characterCount = window.prompt("Enter the count of characters betweek 8 and 128.");
+  console.log(characterCount);
+
+  if (characterCount >= 8 && characterCount <= 128) {
+
+  var capitalLetterCheck = window.confirm("MUST include Capital letters?");
+  console.log(capitalLetterCheck);
+
+  var lowerCaseLetterCheck = window.confirm("MUST include Lower Case letters");
+  console.log(lowerCaseLetterCheck);
+
+  var specialCharacterCheck = window.confirm("MUST  include Special characters?");
+  console.log(specialCharacterCheck);
+
+  var numberCheck = window.confirm("MUST include Numbers?");
+  console.log(numberCheck);
+    
+  }
+  else {
+    window.alert("You did not select the correct number of characters. Try again!")
+    generatePassword();
+  }}
