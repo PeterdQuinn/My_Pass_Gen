4330c3ce9cb7c1a13ab1a7d504c595cdf2fec015
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2021-05-18 01:29:01 -0700

    gnerate pass

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,17 @@
 //START THE PASSWORD GEN///////
 
 var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+
+
+
+// VAR TO POP CHAR IN PASS WORD/////
+
+function generatePassword
