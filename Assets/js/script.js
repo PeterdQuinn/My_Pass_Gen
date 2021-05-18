d2e7ec7a136778a73f03b56bd597bbaebe9b0164
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2021-05-18 01:53:53 -0700

    ready for github

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,4 @@
-//START THE PASSWORD GEN///////
-
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -7,14 +6,9 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
+
 }
-
-
-
-
-// VAR TO POP CHAR IN PASS WORD/////
-
-
+// variables for characters to populate the password
 function generatePassword() {
   let passArray  = [];
   var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
@@ -22,60 +16,62 @@ function generatePassword() {
   var nums = ["0","1","2","3","4","5","6","7","8","9"];
   var specialChars = ["!","$","%","&","?"];
 
-  var passLength = prompt("Password length must be between 8 and 128 characters."); 
-  /// test////
+  var passLength = prompt("Please Choose a  password length between 8 and 128 characters."); 
   console.log(passLength);
 
-}
-
-
-// establish proper password length/////////////
+  // conditional to establish proper password length
   if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
-    window.alert("Please pick the correct format sir or mam");
+    window.alert("Pick a valid option");
     return "";
   } else {
-    window.alert("You selected a password of " + passLength + " characters");
+    window.alert("Your password is " + passLength + " characters");
   }
 
+  // character prompts
+  var lowerConfirm = confirm("YOU NEED  lower case letters in your password");
+  var upperConfirm = confirm("YOU NEED  upper case letters in your password");
+  var numsConfirm = confirm("YOU NEED   numbers in your password");
+  var specialCharsConfirm = confirm("YOU NEED special characters in your password");
 
-
-  /// char propmpts////// 
-
-
-  var lowerConfirm = confirm("YOU NEED to lower case letters in your password");
-  var upperConfirm = confirm("YOU NEED to upper case letters in your password");
-  var numsConfirm = confirm("YOU NEED  numbers in your password");
-  var specialCharsConfirm = confirm("MUST Include special characters in your password");
   var confirmCombined = (lowerConfirm + upperConfirm + numsConfirm + specialCharsConfirm)
 
-
   if (!confirmCombined) {
-    window.alert("WRONG CHOICE OF PASSWORD, TRY AGAIN!!.");
+    window.alert("Please pick at least one valid character option.");
     return "";
   }
 
-// stores the character criteria that is selected via prompts////
+  // stores the character criteria that is selected via prompts
   let selectedCriteria = []
 
-
-// conditionals to decide which characters populate when password is generated////
+  // conditionals to decide which characters populate when password is generated
   if (lowerConfirm === true) {
     selectedCriteria.push(...lowerCase);
   }
-//---------lower cash push---
 
   if (upperConfirm === true) {
     selectedCriteria.push(...upperCase);
   }
-//----- upper case push----/////
 
   if (numsConfirm === true){
     selectedCriteria.push(...nums);
   }
 
-  //-----  nums push---////
-
   if (specialCharsConfirm === true) {
     selectedCriteria.push(...specialChars);
   }
-//--special char push----//////
+
+  for (var i = 0; i < passLength; i++) {
+    var number = Math.floor(Math.random() * Math.floor(selectedCriteria.length));
+    var value = selectedCriteria[number];
+    passArray.push(value);
+  }
+
+  if (passArray.length > 0) {
+    password = passArray.join(''); 
+    return password;
+  }
+  
+};
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
