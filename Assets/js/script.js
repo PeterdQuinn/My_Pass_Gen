617d9c0835e8e2c32362ffe841cbacdd0bc4df00
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2021-05-21 01:11:01 -0700

    cleaning up code

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,33 +1,27 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 
 }
-// variables for characters to populate the password
 function generatePassword() {
   let passArray  = [];
-  var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   var nums = ["0","1","2","3","4","5","6","7","8","9"];
   var specialChars = ["!","$","%","&","?"];
-
-  var passLength = prompt("Please Choose a  password length between 8 and 128 characters."); 
+  var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var passLength = prompt("Password length between 8 and 128 characters."); 
   console.log(passLength);
 
-  // conditional to establish proper password length
   if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
     window.alert("Pick a valid option");
     return "";
   } else {
     window.alert("Your password is " + passLength + " characters");
   }
-
-  // character prompts
   var lowerConfirm = confirm("YOU NEED  lower case letters in your password");
   var upperConfirm = confirm("YOU NEED  upper case letters in your password");
   var numsConfirm = confirm("YOU NEED   numbers in your password");
@@ -36,14 +30,11 @@ function generatePassword() {
   var confirmCombined = (lowerConfirm + upperConfirm + numsConfirm + specialCharsConfirm)
 
   if (!confirmCombined) {
-    window.alert("Please pick at least one valid character option.");
+    window.alert("Pick at least one valid character option.");
     return "";
   }
-
-  // stores the character criteria that is selected via prompts
   let selectedCriteria = []
 
-  // conditionals to decide which characters populate when password is generated
   if (lowerConfirm === true) {
     selectedCriteria.push(...lowerCase);
   }
@@ -72,6 +63,4 @@ function generatePassword() {
   }
   
 };
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
